f4920c966cb60e9fd779644fa362e35757da5efe
perillaroc <perillaroc@gmail.com>
2016-05-11 10:43:36 +0800

    Change celery_config postion.

--- nwpc_monitor_task_scheduler/celery_server/celery.py
+++ nwpc_monitor_task_scheduler/celery_server/celery.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 from celery import Celery
+from nwpc_monitor_task_scheduler.celery_server import celery_config
 
 app = Celery('nwpc_monitor_platform_pipeline')
 
-app.config_from_object('nwpc_monitor_task_scheduler.celery_config')
+app.config_from_object(celery_config)
 
 
 if __name__ == '__main__':
