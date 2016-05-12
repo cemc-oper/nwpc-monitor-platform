5222b6f6cfac75010f640f47573ccc07d84f1b30
perillaroc <perillaroc@gmail.com>
2016-05-12 16:11:46 +0800

    Add argument for run script.

--- nwpc_monitor_broker/run_broker_server.py
+++ nwpc_monitor_broker/run_broker_server.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python
+"""
+run a simple nwpc_monitor_broker server.
+
+Set environment variable MODE to use different config files or use -m/--mode argument to set on command line.
+
+MODE:
+    production: use conf ./conf/production.config.yaml
+    develop: use conf ./conf/develop.config.yaml
+
+"""
+import argparse
 import os
 import sys
 
 if os.environ.has_key('NWPC_MONITOR_PLATFORM_BASE'):
     sys.path.append(os.environ['NWPC_MONITOR_PLATFORM_BASE'])
 
-from nwpc_monitor_broker import app
-
-
 def runserver():
+
+    mode = "production"
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="""\
+DESCRIPTION
+    Run nwpc monitor broker.""")
+
+    parser.add_argument("-m", "--mode",
+        help="run mode, [production, develop]. "
+             "If not set, use mode set in environment variable MODE. "
+             "If there is no such env variable, use default value: production."
+    )
+
+    args = parser.parse_args()
+    if args.mode:
+        os.environ['MODE'] = args.mode
+    elif 'MODE' not in os.environ:
+        os.environ['MODE'] = mode
+
+    from nwpc_monitor_broker import app
+
     app.run(
         host=app.config['BROKER_CONFIG']['host']['ip'],
         port=app.config['BROKER_CONFIG']['host']['port']
--- script/start_broker.sh
+++ script/start_broker.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# run nwpc monitor broker server using develop mode.
+
 base_dir=$(cd "`dirname "$0"`"/..; pwd)
 export NWPC_MONITOR_PLATFORM_BASE=${base_dir}
 
-python ${NWPC_MONITOR_PLATFORM_BASE}/nwpc_monitor_broker/run_broker_server.py
+python ${NWPC_MONITOR_PLATFORM_BASE}/nwpc_monitor_broker/run_broker_server.py --mode=develop
