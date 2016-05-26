0913e85cb80ee055390c628cc5add5b5be873efc
perillaroc <perillaroc@gmail.com>
2016-05-26 16:39:18 +0800

    Fix bug in Model.

--- nwpc_monitor/model/model.py
+++ nwpc_monitor/model/model.py
@@ -2,15 +2,15 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 
-ModelBase = declarative_base()
-
-class Model(ModelBase):
-
+class ModelBase(object):
     def __init__(self):
-        pass
+        self.__table__ = None
 
     def columns(self):
         return [c.name for c in self.__table__.columns]
 
     def to_dict(self):
         return dict([(c, getattr(self, c)) for c in self.columns()])
+
+
+Model = declarative_base(cls=ModelBase)
