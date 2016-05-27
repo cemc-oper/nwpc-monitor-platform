19f2cd22e8320d36d4a37f925ebc14c27f0a2833
perillaroc <perillaroc@gmail.com>
2016-05-27 16:36:02 +0800

    Add some pages for user and org.

--- nwpc_monitor_broker/controller.py
+++ nwpc_monitor_broker/controller.py
@@ -11,4 +11,16 @@ from flask import json, request, jsonify,render_template
 
 @app.route('/')
 def get_index_page():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route('/:owner')
+def get_owner_page(owner):
+    return get_user_page(owner)
+
+
+def get_user_page(user):
+    return render_template("user.html", user=user)
+
+
+def get_org_page(org):
+    return render_template("organization.html", org=org)
