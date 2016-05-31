dfc5ddb7f38862ee8efd58477884e0f18623fb5d
perillaroc <perillaroc@gmail.com>
2016-05-31 09:34:10 +0800

    Add org member list component.

--- nwpc_monitor_broker/static/app/org/org.js
+++ nwpc_monitor_broker/static/app/org/org.js
@@ -1,26 +1,65 @@
 var OrgRepoList = React.createClass({
     getInitialState: function() {
-        return {repo_list: [
-            {name:1},
-            {name:2},
-            {name:3}
-        ]};
+        return {repo_list: []};
     },
     componentDidMount: function() {
         var component = this;
+        component.setState({
+            repo_list: [
+            {name: 'nwpc_op'},
+            {name: 'nwpc_qu'},
+            {name: 'eps_nwpc_qu'}
+        ]});
     },
     render: function() {
         var rows = [];
+        this.state.repo_list.forEach(function (element, index, array) {
+            rows.push(
+                <p key={element.name}>{element.name}</p>
+            )
+        });
         return (
             <div>
-                    {
-                        this.state.repo_list.map(function(an_repo) {
-                            return <p>{an_repo.name}</p>;
-                        })
-                    }
+                {rows}
             </div>
         );
     }
 });
 
 ReactDOM.render(<OrgRepoList />, document.getElementById('org-repo-list-section'));
+
+var OrgMemberList = React.createClass({
+    getInitialState:function() {
+        return {
+            member_list: []
+        };
+    },
+
+    componentDidMount: function() {
+        var component = this;
+        component.setState({
+            member_list:[
+                {name: 'wangdp'},
+                {name: 'cuiyj'},
+                {name: 'wangyt'},
+                {name: 'jiaxzh'}
+            ]
+        })
+    },
+
+    render: function() {
+        var rows = [];
+        this.state.member_list.forEach(function (element, index, array) {
+            rows.push(
+                <p key={element.name}>{element.name}</p>
+            )
+        });
+        return (
+            <div>
+                {rows}
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<OrgMemberList />, document.getElementById('org-member-list-section'));
