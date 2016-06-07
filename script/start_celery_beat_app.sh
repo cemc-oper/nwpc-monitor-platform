83accc6e116cd20de76a4a76653f8344c009b96d
perillaroc <perillaroc@gmail.com>
2016-06-07 13:07:44 +0800

    Add develop mode and pid file path to celery beat script.

--- script/start_celery_beat_app.sh
+++ script/start_celery_beat_app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 base_dir=$(cd "`dirname "$0"`"/..; pwd)
+export NWPC_MONITOR_PLATFORM_BASE=${base_dir}
 
-celery --workdir=${base_dir} --app=nwpc_monitor_task_scheduler.celery_server beat
+export MODE=develop
+
+celery --workdir=${base_dir} --app=nwpc_monitor_task_scheduler.celery_server -s /tmp/celerybeat-schedule beat
