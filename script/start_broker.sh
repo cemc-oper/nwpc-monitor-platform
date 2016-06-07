d9b8a95b0659acebc7015992b14088a3eb919eb8
perillaroc <perillaroc@gmail.com>
2016-06-07 15:21:19 +0800

    Use python3 in run script.

--- script/start_broker.sh
+++ script/start_broker.sh
@@ -5,4 +5,4 @@
 base_dir=$(cd "`dirname "$0"`"/..; pwd)
 export NWPC_MONITOR_PLATFORM_BASE=${base_dir}
 
-python ${NWPC_MONITOR_PLATFORM_BASE}/nwpc_monitor_broker/run_broker_server.py --mode=develop
+python3 ${NWPC_MONITOR_PLATFORM_BASE}/nwpc_monitor_broker/run_broker_server.py --mode=develop
