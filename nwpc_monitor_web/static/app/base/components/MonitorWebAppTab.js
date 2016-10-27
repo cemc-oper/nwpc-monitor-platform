47b8f2a4d803716946753a79d203b36729b5a398
perillaroc <perillaroc@gmail.com>
2016-10-27 16:41:26 +0800

    Add an icon from weui for tabbar.
    从 weui 项目拷贝图标。

--- nwpc_monitor_web/static/app/base/components/MonitorWebAppTab.js
+++ nwpc_monitor_web/static/app/base/components/MonitorWebAppTab.js
@@ -9,19 +9,19 @@ export default class MonitorWebAppTab extends Component{
         return (
             <div className="weui-tabbar">
                 <a href="/" className="weui-tabbar__item weui-bar__item_on">
-                    <img src="/static/images/icon_tabbar.png" alt="" className="weui-tabbar__icon" />
+                    <img src="/static/image/icon_tabbar.png" alt="" className="weui-tabbar__icon" />
                     <p className="weui-tabbar__label">系统</p>
                 </a>
                 <a href="/hpc/disk-usage" className="weui-tabbar__item">
-                    <img src="/static/images/icon_tabbar.png" alt="" className="weui-tabbar__icon" />
+                    <img src="/static/image/icon_tabbar.png" alt="" className="weui-tabbar__icon" />
                     <p className="weui-tabbar__label">空间</p>
                 </a>
                 <a href="/hpc/loadleveler" className="weui-tabbar__item">
-                    <img src="/static/images/icon_tabbar.png" alt="" className="weui-tabbar__icon" />
+                    <img src="/static/image/icon_tabbar.png" alt="" className="weui-tabbar__icon" />
                     <p className="weui-tabbar__label">队列</p>
                 </a>
                 <a href="/about" className="weui-tabbar__item">
-                    <img src="/static/images/icon_tabbar.png" alt="" className="weui-tabbar__icon" />
+                    <img src="/static/image/icon_tabbar.png" alt="" className="weui-tabbar__icon" />
                     <p className="weui-tabbar__label">关于</p>
                 </a>
             </div>
