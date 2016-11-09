cae53e1c2ca5991e8b80ecd08dfac6e6726b4f52
perillaroc <perillaroc@gmail.com>
2016-11-09 20:45:47 +0800

    Add sorting.

--- nwpc_monitor_web/static/app/hpc/containers/HpcLoadlevelerStatusApp.js
+++ nwpc_monitor_web/static/app/hpc/containers/HpcLoadlevelerStatusApp.js
@@ -10,11 +10,36 @@ import LoadingToast from '../../base/components/LoadingToast'
 import { Util } from '../../base/util/util'
 
 class HpcLoadlevelerStatusApp extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            sort_label: null,
+            is_asc_order: true
+        }
+    }
+
     componentDidMount(){
         const { dispatch, params } = this.props;
         let user = params.user;
 
         dispatch(fetchHpcUserLoadlevelerStatus(user));
+    }
+
+    handleSortClick(sort_label){
+        if(this.state.sort_label===null){
+            this.setState({
+                sort_label: sort_label,
+                is_asc_order: true
+            })
+        } else if(this.state.sort_label == sort_label) {
+            this.setState({
+                is_asc_order: !this.state.is_asc_order
+            });
+        } else {
+            this.setState({
+                sort_label: sort_label
+            })
+        }
     }
 
     static findPropById(job, id){
@@ -35,20 +60,86 @@ class HpcLoadlevelerStatusApp extends Component{
         return text;
     }
 
+    static compareString(a,b){
+        if(a<b)
+            return -1;
+        else if(a>b)
+            return 1;
+        else
+            return 0;
+
+    }
+
+    static compareJobStatus(a, b){
+        return HpcLoadlevelerStatusApp.compareString(
+            HpcLoadlevelerStatusApp.getPropTextById(a, "llq.status"),
+            HpcLoadlevelerStatusApp.getPropTextById(b, "llq.status")
+        );
+    }
+
+    static compareOwner(a, b){
+        return HpcLoadlevelerStatusApp.compareString(
+            HpcLoadlevelerStatusApp.getPropTextById(a, "llq.owner"),
+            HpcLoadlevelerStatusApp.getPropTextById(b, "llq.owner")
+        );
+    }
+
+    static compareQueueDate(a, b){
+        return HpcLoadlevelerStatusApp.compareString(
+            HpcLoadlevelerStatusApp.getPropTextById(a, "llq.queue_date"),
+            HpcLoadlevelerStatusApp.getPropTextById(b, "llq.queue_date")
+        );
+    }
+
+    static compareJobClass(a, b){
+        return HpcLoadlevelerStatusApp.compareString(
+            HpcLoadlevelerStatusApp.getPropTextById(a, "llq.class"),
+            HpcLoadlevelerStatusApp.getPropTextById(b, "llq.class")
+        );
+    }
+
     render() {
         const { params, loadleveler_status } = this.props;
-        console.log(loadleveler_status);
+
+        let user = params.user;
 
         let { collect_time, jobs } = loadleveler_status;
+        let local_jobs = jobs.concat();
 
-        let jobs_node = jobs.map(function(a_job, index){
+        switch(this.state.sort_label){
+            case "llq.owner":
+                if(this.state.is_asc_order)
+                    local_jobs.sort(HpcLoadlevelerStatusApp.compareOwner);
+                else
+                    local_jobs.sort((a,b)=>(-1)*HpcLoadlevelerStatusApp.compareOwner(a,b));
+                break;
+            case "llq.queue_date":
+                if(this.state.is_asc_order)
+                    local_jobs.sort(HpcLoadlevelerStatusApp.compareQueueDate);
+                else
+                    local_jobs.sort((a,b)=>(-1)*HpcLoadlevelerStatusApp.compareQueueDate(a,b));
+                break;
+            case "llq.status":
+                if(this.state.is_asc_order)
+                    local_jobs.sort(HpcLoadlevelerStatusApp.compareJobStatus);
+                else
+                    local_jobs.sort((a,b)=>(-1)*HpcLoadlevelerStatusApp.compareJobStatus(a,b));
+                break;
+            case "llq.class":
+                if(this.state.is_asc_order)
+                    local_jobs.sort(HpcLoadlevelerStatusApp.compareJobClass);
+                else
+                    local_jobs.sort((a,b)=>(-1)*HpcLoadlevelerStatusApp.compareJobClass(a,b));
+                break;
+            default:
+                break;
+        }
+
+        let jobs_node = local_jobs.map(function(a_job, index){
             let id = HpcLoadlevelerStatusApp.getPropTextById(a_job, "llq.id");
             let owner = HpcLoadlevelerStatusApp.getPropTextById(a_job, "llq.owner");
             let queue_date = HpcLoadlevelerStatusApp.getPropTextById(a_job, "llq.queue_date");
             let status = HpcLoadlevelerStatusApp.getPropTextById(a_job, "llq.status");
-            // if(status.length==1){
-            //     status += " ";
-            // }
             let ll_class = HpcLoadlevelerStatusApp.getPropTextById(a_job, "llq.class");
             let job_script = HpcLoadlevelerStatusApp.getPropTextById(a_job, "llq.job_script");
             return (
@@ -73,10 +164,22 @@ class HpcLoadlevelerStatusApp extends Component{
                     <div className="weui-cell">
                         <div className="weui-cell__bd">
                             <p className="loadleveler-status-row">
-                                <span className="loadleveler-status-cell-status">ST</span>
-                                <span className="loadleveler-status-cell-owner">Owner</span>
-                                <span className="loadleveler-status-cell-class">Class</span>
-                                <span className="loadleveler-status-cell-queue-date">Queue Date</span>
+                                <span className="loadleveler-status-cell-status">
+                                    <a href="javascript:;" onClick={this.handleSortClick.bind(this, "llq.status")}
+                                       className="loadleveler-status-header-button">ST</a>
+                                </span>
+                                <span className="loadleveler-status-cell-owner">
+                                    <a href="javascript:;" onClick={this.handleSortClick.bind(this, "llq.owner")}
+                                       className="loadleveler-status-header-button">Owner</a>
+                                </span>
+                                <span className="loadleveler-status-cell-class">
+                                    <a href="javascript:;" onClick={this.handleSortClick.bind(this, "llq.class")}
+                                       className="loadleveler-status-header-button">Class</a>
+                                </span>
+                                <span className="loadleveler-status-cell-queue-date">
+                                    <a href="javascript:;" onClick={this.handleSortClick.bind(this, "llq.queue_date")}
+                                       className="loadleveler-status-header-button">Queue Date</a>
+                                </span>
                             </p>
                         </div>
                     </div>
