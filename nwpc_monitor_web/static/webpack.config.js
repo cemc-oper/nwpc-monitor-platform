3f5cd5586991db79941cce0e9abae48908a5b264
perillaroc <perillaroc@gmail.com>
2016-11-11 16:49:36 +0800

    add production build config. move some js library from bundle to smaller bundle size.

--- nwpc_monitor_web/static/webpack.config.js
+++ nwpc_monitor_web/static/webpack.config.js
@@ -1,32 +1,66 @@
 var path = require('path');
+var webpack = require('webpack');
 
-module.exports = {
-    devtool: "source-map",
-    entry: {
-        welcome: './app/welcome/index.js',
-        'operation-system': './app/operation-system/index.js',
-        'hpc': './app/hpc/index.js'
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: "[name].entry.js",
-        sourceMapFilename: '[file].map'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: [ 'babel' ],
-                exclude: /node_modules/,
-                include: __dirname
-            }
-        ]
-    },
-    externals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM',
-        'redux': 'Redux',
-        'react-redux': 'ReactRedux',
-        'echarts': 'echarts'
-    }
+
+var entry= {
+    welcome: './app/welcome/index.js',
+    'operation-system': './app/operation-system/index.js',
+    'hpc': './app/hpc/index.js'
 };
+
+var webpack_module= {
+    loaders: [
+        {
+            test: /\.js$/,
+            loaders: [ 'babel' ],
+            exclude: /node_modules/,
+            include: __dirname
+        }
+    ]
+};
+
+var externals= {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'redux': 'Redux',
+    'react-redux': 'ReactRedux',
+    'echarts': 'echarts',
+    'moment': 'moment',
+    'react-router': 'ReactRouter',
+    'react-router-redux': 'ReactRouterRedux',
+    'redux-thunk': 'ReduxThunk'
+};
+
+if( process.env.NODE_ENV === 'production' ) {
+
+    module.exports = {
+        entry: entry,
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: "[name].entry.min.js",
+            sourceMapFilename: '[file].map'
+        },
+        module: webpack_module,
+        externals: externals,
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                test: /(\.jsx|\.js)$/,
+                compress: {
+                    warnings: false
+                },
+            })
+        ]
+    };
+}else{
+    module.exports = {
+        devtool: "source-map",
+        entry: entry,
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: "[name].entry.js",
+            sourceMapFilename: '[file].map'
+        },
+        module: webpack_module,
+        externals: externals
+    };
+}
