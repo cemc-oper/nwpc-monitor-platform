a466377d50d49eae05efaad3adcda25a71999e11
perillaroc <perillaroc@gmail.com>
2017-02-28 16:22:03 +0800

    add more test data ofr pruning algorithm.

--- test/manual/nwpc_monitor_broker/tree_algo/pruning_algorithm.py
+++ test/manual/nwpc_monitor_broker/tree_algo/pruning_algorithm.py
@@ -1,7 +1,10 @@
 import os
 import json
+import datetime
 
-from nwpc_work_flow_model.sms.visitor import pre_order_travel_dict, NodeVisitor
+from nwpc_work_flow_model.sms.bunch import Bunch
+from nwpc_work_flow_model.sms.visitor import \
+    pre_order_travel_dict, pre_order_travel, NodeVisitor, ErrorStatusTaskVisitor
 
 
 class NodeCountVisitor(NodeVisitor):
@@ -23,14 +26,52 @@ class NodeCountVisitor(NodeVisitor):
         self.level -= 1
 
 
+class PruningErrorStatusTaskVisitor(NodeVisitor):
+    def __init__(self):
+        NodeVisitor.__init__(self)
+        self.level = 0
+        self.error_task_list = []
+
+    def visit(self, node):
+        if node.status in ['que', 'act', 'com']:
+            node.children = []
+            return
+
+        if node.status == 'abo' and node.is_leaf():
+            self.error_task_list.append(node)
+
+    def before_visit_child(self):
+        self.level += 1
+
+    def after_visit_child(self):
+        self.level -= 1
+
+
 def main():
-    file_path = os.path.join(os.path.dirname(__file__) + "/../data/data_eps_nwpc_qu.json")
+    file_path = os.path.join(os.path.dirname(__file__) + "/../data/data_long.json")
     with open(file_path, 'r') as f:
         data = json.load(f)
         status_dict = data["data"]["content"]["status"]
         visitor = NodeCountVisitor()
         pre_order_travel_dict(status_dict, visitor)
         print(visitor.max_level, visitor.count)
+
+        tree = Bunch.create_from_dict(status_dict)
+        start_time = datetime.datetime.now()
+        error_visitor = ErrorStatusTaskVisitor()
+        pre_order_travel(tree, error_visitor)
+        end_time = datetime.datetime.now()
+        print(len(error_visitor.error_task_list))
+        print(end_time - start_time)
+
+        tree = Bunch.create_from_dict(status_dict)
+        start_time = datetime.datetime.now()
+        error_visitor = PruningErrorStatusTaskVisitor()
+        pre_order_travel(tree, error_visitor)
+        end_time = datetime.datetime.now()
+        print(len(error_visitor.error_task_list))
+        print(end_time - start_time)
+
         print("end")
 
 
