b314906ccfa360f0a2fc81c6cdb9861b8e2762b3
perillaroc <perillaroc@gmail.com>
2017-03-31 16:52:30 +0800

    add get_sms_node_task.

--- nwpc_monitor_task_scheduler/celery_server/task/sms.py
+++ nwpc_monitor_task_scheduler/celery_server/task/sms.py
@@ -62,3 +62,63 @@ def get_group_sms_status_task():
     g = group(get_sms_status_task.s(a_repo) for a_repo in repos)
     result = g.delay()
     return
+
+
+@app.task()
+def get_sms_node_task(args):
+    host = args['auth']['host']
+    port = args['auth']['port']
+    user = args['auth']['user']
+    password = args['auth']['password']
+
+    sms_server = args['sms']['sms_server']
+    sms_user = args['sms']['sms_user']
+    sms_password = args['sms']['sms_password']
+    sms_node = args['sms']['sms_node']
+
+    config_dict = task_config.config
+    project_dir = config_dict['sms_node_task']['project']['dir']
+    project_program = config_dict['sms_node_task']['project']['program']
+    project_script = config_dict['sms_node_task']['project']['script']
+
+    env_hosts = ['{user}@{host}'.format(user=user, host=host)]
+    env_password = '{password}'.format(password=password)
+
+    env.hosts = env_hosts
+    env.password = env_password
+
+    def get_sms_node(sms_server, sms_user, sms_password, sms_node):
+        with cd(project_dir):
+            result = run(
+                "{program} {script} --sms-server={sms_server} "
+                "--sms-user={sms_user} --sms-password {sms_password} --node-path={sms_node}"
+                .format(
+                    program=project_program,
+                    script=project_script,
+                    sms_server=sms_server,
+                    sms_user=sms_user,
+                    sms_password=sms_password,
+                    sms_node=sms_node
+                ))
+            response_json_string = '\n'.join(result.splitlines()[1:])
+            print(response_json_string)
+
+    execute(get_sms_node, sms_server=sms_server, sms_user=sms_user, sms_password=sms_password, sms_node=sms_node)
+
+
+if __name__ == "__main__":
+    args = {
+        'auth': {
+            'host': 'uranus.hpc.nmic.cn',
+            'port': '22',
+            'user': 'wangdp',
+            'password': '***REMOVED***'
+        },
+        'sms': {
+            'sms_server': 'nwpc_wangdp',
+            'sms_user': 'wangdp',
+            'sms_password': '1',
+            'sms_node': '/windroc_info'
+        }
+    }
+    get_sms_node_task(args)
