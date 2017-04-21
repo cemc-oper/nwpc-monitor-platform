b34818b5c55320ccab7feacbf8acac5f7b51f730
perillaroc <perillaroc@gmail.com>
2017-04-21 14:53:05 +0800

    add test for loadleveler celery task.

---
+++ test/manual/nwpc_monitor_task_scheduler/run_loadleveler_task.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__)+"/../../../")
+os.environ['MODE'] = 'develop'
+from nwpc_monitor_task_scheduler.celery_server.task import hpc
+
+
+def test_loadleveler_task():
+    param = {
+        'user': 'wangdp',
+        'password': '***REMOVED***',
+        'host': 'uranus.hpc.nmic.cn',
+        'port': 22
+    }
+    hpc.get_hpc_loadleveler_usage(param)
+
+
+if __name__ == "__main__":
+    test_loadleveler_task()
