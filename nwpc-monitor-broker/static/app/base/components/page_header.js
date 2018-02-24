ca074dbe545afa91b0a8fb42a58fd8180fb809ac
perillaroc <perillaroc@gmail.com>
2018-02-24 14:22:47 +0800

    update page_header. #20

--- nwpc-monitor-broker/static/app/base/components/page_header.js
+++ nwpc-monitor-broker/static/app/base/components/page_header.js
@@ -1,57 +1,52 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class PageHeader extends Component{
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        let index_page_url = this.props.url.index_page; // {{ url_for('get_index_page') }}
-        return (
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#monitor-broker-navbar-collapse" aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a className="navbar-brand" href={ index_page_url }>NWPC业务监控平台</a>
-                    </div>
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let index_page_url = this.props.url.index_page; // {{ url_for('get_index_page') }}
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href={ index_page_url }>NWPC业务监控平台</a>
 
-                    <div className="collapse navbar-collapse" id="monitor-broker-navbar-collapse">
-                        <form className="navbar-form navbar-left" role="search">
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Search Monitor" />
-                            </div>
-                        </form>
-                        <ul className="nav navbar-nav">
-                            <li><a href={ index_page_url }>首页 <span className="sr-only">(current)</span></a></li>
-                            <li><a href="#">功能</a></li>
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><a href="#">帮助</a></li>
-                            <li className="dropdown">
-                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">用户 <span className="caret"></span></a>
-                                <ul className="dropdown-menu">
-                                    <li><a href="#">主页</a></li>
-                                    <li><a href="#">帮助</a></li>
-                                    <li role="separator" className="divider"></li>
-                                    <li><a href="#">设置</a></li>
-                                    <li><a href="#">注销</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        );
-    }
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href={ index_page_url }>首页</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">功能</a>
+          </li>
+        </ul>
+
+        <form className="form-inline">
+          <input className="form-control" type="text" placeholder="Search" />
+        </form>
+
+        <ul className="navbar-nav pull-right">
+          <li className="nav-item">
+            <a className="nav-link" href="#">帮助</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="page_header_nav_user_drop_down"
+               data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">用户</a>
+            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="page_header_nav_user_drop_down">
+              <a className="dropdown-item" href="#">主页</a>
+              <a className="dropdown-item" href="#">帮助</a>
+              <div className="dropdown-divider" />
+              <a className="dropdown-item" href="#">设置</a>
+              <a className="dropdown-item" href="#">注销</a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 
 PageHeader.propTypes = {
-    url: PropTypes.objectOf(PropTypes.string).isRequired
+  url: PropTypes.objectOf(PropTypes.string).isRequired
 };
 
 
