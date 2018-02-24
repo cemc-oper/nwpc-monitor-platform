a8b06d517e2143609c323edfbcaaa7c171e9df29
perillaroc <perillaroc@gmail.com>
2018-02-24 14:54:19 +0800

    update OrgAppTitle. #20

--- nwpc-monitor-broker/static/app/org/components/OrgAppTitle.js
+++ nwpc-monitor-broker/static/app/org/components/OrgAppTitle.js
@@ -1,22 +1,25 @@
 import React, { Component, PropTypes } from 'react';
 
+const octicons = require("octicons");
+
 export default class OrgAppTitle extends Component{
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        let owner = this.props.owner;
-        return (
-            <section className="row">
-                <h1>
-                    <span className="glyphicon glyphicon-king" aria-hidden="true" />
-                    <a href={ '/' + owner }>{ owner }</a>
-                </h1>
-            </section>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let owner = this.props.owner;
+    const org_icon_node = octicons.organization.toSVG({ "width": 30 });
+    return (
+      <section className="row">
+        <h1>
+          <span dangerouslySetInnerHTML={{__html: org_icon_node}} />
+          <a href={ '/' + owner }>{ owner }</a>
+        </h1>
+      </section>
+    );
+  }
 }
 
 OrgAppTitle.propTypes = {
-    owner: PropTypes.string.isRequired
+  owner: PropTypes.string.isRequired
 };
