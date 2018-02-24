d89dae3ddf3574f6dc3d6ce61a38c7fe88d950ed
perillaroc <perillaroc@gmail.com>
2018-02-24 15:19:13 +0800

    update OrgReposApp.

--- nwpc-monitor-broker/static/app/org/components/OrgRepoList.js
+++ nwpc-monitor-broker/static/app/org/components/OrgRepoList.js
@@ -2,32 +2,32 @@ import React, { Component, PropTypes } from 'react';
 
 /* component */
 export default class OrgRepoList extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        //console.log('repo_list', this.props.repo_list);
-        let owner = this.props.owner;
-        return (
-            <div>
-                {this.props.repo_list.map((repo, index) =>
-                    <div className="panel panel-default" key={repo.id}>
-                        <div className="panel-body">
-                            <a href={ '/' + owner +'/'+ repo.name }>{repo.name}</a>
-                            <p>{ repo.description }</p>
-                            <p>最后更新时间：{ repo.update_time }</p>
-                        </div>
-                    </div>
-                )}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    //console.log('repo_list', this.props.repo_list);
+    let owner = this.props.owner;
+    return (
+      <div>
+        {this.props.repo_list.map((repo, index) =>
+          <div className="card" key={repo.id}>
+            <div className="card-body">
+              <a href={ '/' + owner +'/'+ repo.name }>{repo.name}</a>
+              <p>{ repo.description }</p>
+              <p>最后更新时间：{ repo.update_time }</p>
             </div>
-        );
-    }
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 OrgRepoList.propTypes = {
-    repo_list: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired
-    }).isRequired).isRequired,
+  repo_list: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired).isRequired,
 
-    owner: PropTypes.string.isRequired
+  owner: PropTypes.string.isRequired
 };
--- nwpc-monitor-broker/static/app/org/routes.js
+++ nwpc-monitor-broker/static/app/org/routes.js
@@ -9,14 +9,14 @@ import OrgSettingsApp from './containers/OrgSettingsApp'
 import DingTalkWarningApp from './containers/warning/DingTalkWarningApp'
 
 export default (
-    <Route path="/:owner" component={OrgApp}>
-        <IndexRoute component={OrgReposApp} />
-        <Route path="/orgs/:owner/repos" component={OrgReposApp} />
-        <Route path="/orgs/:owner/people" component={OrgPeopleApp} />
-        <Route path="/orgs/:owner/warning" component={OrgWarningApp} >
-            <IndexRoute component={DingTalkWarningApp} />
-            <Route path="ding_talk" component={DingTalkWarningApp} />
-        </Route>
-        <Route path="/orgs/:owner/settings" component={OrgSettingsApp} />
+  <Route path="/:owner" component={OrgApp}>
+    <IndexRoute component={OrgReposApp} />
+    <Route path="/orgs/:owner/repos" component={OrgReposApp} />
+    <Route path="/orgs/:owner/people" component={OrgPeopleApp} />
+    <Route path="/orgs/:owner/warning" component={OrgWarningApp} >
+      <IndexRoute component={DingTalkWarningApp} />
+      <Route path="ding_talk" component={DingTalkWarningApp} />
     </Route>
+    <Route path="/orgs/:owner/settings" component={OrgSettingsApp} />
+  </Route>
 )
