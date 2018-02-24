79d4da604d03da69ab829de21c9d52e39c083870
perillaroc <perillaroc@gmail.com>
2018-02-24 12:46:07 +0800

    update welcome app to boostrap v4. #20

--- nwpc-monitor-broker/static/webpack.config.js
+++ nwpc-monitor-broker/static/webpack.config.js
@@ -1,32 +1,36 @@
 const path = require('path');
 
 module.exports = {
-    devtool: "source-map",
-    entry: {
-        welcome: './app/welcome/welcome.js',
-        org: [
-            './app/org/index.js'
-        ],
-        repo: [
-            './app/repo/index.js'
-        ],
-        user: './app/user/index.js'
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: "[name].entry.js",
-        sourceMapFilename: '[file].map'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader?cacheDirectory=true'
-                },
-                exclude: /node_modules/,
-                include: __dirname
-            }
-        ]
-    }
+  devtool: "source-map",
+  entry: {
+    welcome: './app/welcome/welcome.js',
+    org: [
+      './app/org/index.js'
+    ],
+    repo: [
+      './app/repo/index.js'
+    ],
+    user: './app/user/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "[name].entry.js",
+    sourceMapFilename: '[file].map'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader?cacheDirectory=true'
+        },
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 };
