ca01af1762ccf70a22e52511f39c5ecb9e68fbbc
perillaroc <perillaroc@gmail.com>
2018-07-10 15:22:17 +0800

    add ecflow support in task scheduler. #27
    comment sms node tasks in code. Future work is required.

--- nwpc_monitor_task_scheduler/celery_server/task/__init__.py
+++ nwpc_monitor_task_scheduler/celery_server/task/__init__.py
@@ -2,3 +2,4 @@ from nwpc_monitor_task_scheduler.celery_server.task.sms import *
 from nwpc_monitor_task_scheduler.celery_server.task.cloud import *
 from nwpc_monitor_task_scheduler.celery_server.task.hpc import *
 from nwpc_monitor_task_scheduler.celery_server.task.celery_connect import *
+from nwpc_monitor_task_scheduler.celery_server.task.ecflow import *
