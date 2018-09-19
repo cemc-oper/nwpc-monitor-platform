7b6e3b7c760fc8a783376255762fb97df9775408
perillaroc <perillaroc@gmail.com>
2018-09-19 11:08:32 +0800

    add import statements in rmdb.__init__.py

--- nmp-model/nmp_model/rmdb/__init__.py
+++ nmp-model/nmp_model/rmdb/__init__.py
@@ -1,1 +1,10 @@
 # coding: utf-8
+from .model import Model
+from .owner import Owner
+from .user import User
+from .org import Org
+from .repo import Repo
+from .org_user import OrgUser
+from .dingtalk_user import DingtalkUser
+from .dingtalk_warn_watch import DingtalkWarnWatch
+from .util import Util
