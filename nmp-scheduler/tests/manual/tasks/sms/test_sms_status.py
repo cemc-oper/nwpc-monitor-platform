acd5e34fab9554310bd6600906e29af2ce39f75f
perillaroc <perillaroc@gmail.com>
2018-12-21 09:27:41 +0800

    :bug: check status message app type and use SMS or ecflow workflow. fix #48
    :loud_sound: add more prints in status message handler.

--- nmp-scheduler/tests/manual/tasks/sms/test_sms_status.py
+++ nmp-scheduler/tests/manual/tasks/sms/test_sms_status.py
@@ -6,10 +6,10 @@ def test_sms_status_task():
     args = {
         'owner': 'nwp_xp',
         'repo': 'nwpc_op',
-        'sms_host': '10.20.49.131',
-        'sms_prog': '310066',
-        'sms_user': 'nwp',
-        'sms_name': 'nwpc_op',
+        'sms_host': '10.20.49.115',
+        'sms_prog': '68408705',
+        'sms_user': 'wangdp',
+        'sms_name': 'nwpc_wangdp',
     }
     # print(json.dumps(get_ecflow_status_task(args), indent=2))
     result = get_sms_status_task.delay(args)
